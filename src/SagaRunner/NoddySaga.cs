c899bebeb43191671f97246cd9ec9b8344eeb620
Mark Lysaght <mark.lysaght@gmail.com>
2012-09-06 08:57:53 +0100

    Added licence header

--- src/SagaRunner/NoddySaga.cs
+++ src/SagaRunner/NoddySaga.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// Copyright 2012 Zarion Ltd.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +31,9 @@ namespace SagaRunner
             Map(s => s.CorrelationId).Column("CorrelationId").Key(DapperExtensions.Mapper.KeyType.Assigned);
             Map(s => s.NoddyText).Column("NoddyText");
             Map(s => s.BigEarText).Column("BigEarText");
+            Map(s => s.CurrentState.Enter.Name).Column("StateEnter");
+            Map(s => s.CurrentState.Leave.Name).Column("StateLeave");
+            Map(s => s.CurrentState.Name).Column("StateName");
         }
     }
 
--- src/SagaRunner/Program.cs
+++ src/SagaRunner/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// Copyright 2012 Zarion Ltd.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+
+using System;
 using Dapper;
 using MassTransit;
 using MassTransit.Saga;
@@ -61,6 +74,10 @@ CREATE TABLE [dbo].[NoddySaga]
     [CorrelationId] [uniqueidentifier] NOT NULL,
     [NoddyText] [nvarchar](50) NULL,
     [BigEarText] [nvarchar](255) NULL,
+    [ReadyFlags] [int] not NULL,
+    [StateEnter] [nvarchar](255) NULL,
+    [StateLeave] [nvarchar](255) NULL,
+    [StateName] [nvarchar](255) NULL,
 PRIMARY KEY (CorrelationId)
 )
 create unique index NoddySaga_pk on NoddySaga (CorrelationId)");
